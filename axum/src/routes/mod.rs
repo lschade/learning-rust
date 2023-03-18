3a43ed98cf41a3c615d9c8563264e616c8c3d927
Lukas Schade <lukas.schade@rwth-aachen.de>
2023-03-18 13:01:24 +0100

    I'm a teapot

--- axum/src/routes/mod.rs
+++ axum/src/routes/mod.rs
@@ -3,6 +3,7 @@ mod string_body;
 mod params;
 mod custom_middleware;
 mod set_middleware_custom_header;
+mod teapot;
 
 use axum::{
     body::Body,
@@ -15,6 +16,7 @@ use string_body::json_body;
 use params::path_variable;
 use custom_middleware::custom_middleware;
 use set_middleware_custom_header::read_custom_header;
+use teapot::im_a_teapot;
 
 use self::params::query_params;
 
@@ -26,5 +28,6 @@ pub fn get_routes() -> Router<(), Body> {
         .route("/path/:id/:number", get(path_variable))
         .route("/query", get(query_params))
         .route("/middleware", get(custom_middleware))
+        .route("/teapot", get(im_a_teapot))
         .route_layer(middleware::from_fn(read_custom_header))
 }
---
+++ axum/src/routes/teapot.rs
@@ -0,0 +1,6 @@
+use axum::http::StatusCode;
+
+
+pub async fn im_a_teapot() -> Result<(), StatusCode> {
+    return Err(StatusCode::IM_A_TEAPOT)
+}
